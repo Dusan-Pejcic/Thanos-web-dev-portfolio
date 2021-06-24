474b0841eef0e4f7c69867f25a2751a8ab045a9e
dusan pejcic <dushan.pejcic@googlemail.com>
2021-06-24 02:48:01 +0200

    navigation on small viewport added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(function(){
-      $('#menu').slicknav();
-  });
+console.log('hoooiiii');
+
+const hamburger = document.querySelector('.hamburger');
+const menuCloseBtn = document.querySelector('.close');
+const menu = document.querySelector('.menu');
+hamburger.addEventListener('click', () => {
+    menu.classList.add('clicked');
+});
+menuCloseBtn.addEventListener('click', () => {
+    menu.classList.remove('clicked');
+});
